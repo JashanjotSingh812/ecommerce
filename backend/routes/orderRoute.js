69ab5862636564bc64452c08022b4662ede3438d
JashanjotSingh812 <122783310+JashanjotSingh812@users.noreply.github.com>
2025-05-31 12:19:23 +0530

    Add files via upload

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { placeOrder, placeOrderRazorpay, placeOrderStripe, updateStatus, allOrders, userOrders } from '../controllers/orderController.js'
+import { placeOrder, placeOrderRazorpay, placeOrderStripe, updateStatus, allOrders, userOrders, verifyStripe, verifyRazorPay } from '../controllers/orderController.js'
 import adminAuth from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js'
 
@@ -12,9 +12,23 @@ orderRouter.post('/status', adminAuth, updateStatus)
 // Payment features
 orderRouter.post('/place', authUser, placeOrder)
 orderRouter.post('/stripe', authUser, placeOrderStripe)
-// orderRouter.post('/razorpay', authUser, placeOrderRazorpay)
+orderRouter.post('/razorpay', authUser, placeOrderRazorpay)
 
 // User features
 orderRouter.post('/userorders', authUser, userOrders)
+
+// 
+orderRouter.use((req, res, next) => {
+  console.log(`📦 orderRouter hit at ${req.method} ${req.originalUrl}`);
+  next();
+});
+
+orderRouter.post('/verifyStripe', authUser, (req, res, next) => {
+  console.log('✅ verifyStripe route hit');
+  next(); // Let it move to the controller
+});
+
+orderRouter.post('/verifyStripe',authUser,verifyStripe)
+orderRouter.post('/verifyRazorpay',authUser,verifyRazorPay)
 
 export default orderRouter;
