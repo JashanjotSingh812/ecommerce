fce0f110b0184ac14dc8c4dd2c509edbf0ea5387
JashanjotSingh812 <122783310+JashanjotSingh812@users.noreply.github.com>
2025-05-31 12:22:01 +0530

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
-import Login from './pages/Login';
+import Login from './Pages/Login';
 import PlaceOrder from './pages/PlaceOrder';
 import Orders from './pages/Orders';
 import Collection from './pages/Collection'; 
@@ -14,6 +14,7 @@ import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Verify from './Pages/Verify';
 
 
 const App = () => {
@@ -32,6 +33,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/orders" element={<Orders />} />
+        <Route path="/verify" element={<Verify/>} />
       </Routes>
       <Footer/>
     </div>
